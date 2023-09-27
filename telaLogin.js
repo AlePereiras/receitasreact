669381b7763e6f951f58e5b163a0a4bf1e9ea9a4
ale <128554136+AlePereiras@users.noreply.github.com>
2023-09-27 13:56:39 -0300

    instalando o react native paper e fazendo a tela de login

--- telaLogin.js
+++ telaLogin.js
@@ -1,3 +1,46 @@
-import { Text, TextInput, Button, View, StyleSheet } from "react-native";
+import { Text, TextInput, View, StyleSheet } from "react-native";
 import React, { useState } from "react";
+import { Button } from "react-native-paper";
+import * as React from 'react-native-paper';
 
+const input = () => {
+    const [input1, setInput1] = useState('');
+    const [input2, setInput2] = useState('');
+    const [input3, setInput3] = useState('');
+    const [cliques, setCliques] = useState(0);
+
+    return (
+        <View style={{marginTop: 340, }}>
+            <Text style={{marginLeft: 140,}}>Cadastrar Usuário</Text>
+
+            <TextInput onChangeText={(texto1) => setInput1(texto1)}
+                defaultValue={input1} placeholder="Coloque seu nome..."
+                style={{
+                    marginTop: 10,
+                }}
+            />
+
+            <TextInput onChangeText={(texto2) => setInput2(texto2)}
+                defaultValue={input2} placeholder="Coloque seu Email..."
+                style={{
+                    marginTop: 10,
+                }}
+            />
+
+            <TextInput onChangeText={(texto3) => setInput3(texto3)}
+                defaultValue={input3} placeholder="Coloque sua Senha..."
+                style={{
+                    marginTop: 10,
+                }}
+            />
+
+            <Button title="Cadastrar" onPress={() => {
+                setCliques();
+            }}
+            />
+
+        </View>
+    );
+}
+
+export default input;
