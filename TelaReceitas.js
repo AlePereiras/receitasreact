1c8ad4ef431545c8e6ced851583faabe470a30d9
roberto_farias <128554136+AlePereiras@users.noreply.github.com>
2023-11-01 16:17:40 -0300

    Conectando o banco no front

--- TelaReceitas.js
+++ TelaReceitas.js
@@ -37,66 +37,50 @@ const Receitas = () => {
                 }}>Destaques</Text>
             </View>
                 
-            <FlatListBasics></FlatListBasics>
+            {/* <FlatListBasics></FlatListBasics> */}
+    <Receita></Receita>
 
         </PaperProvider>
     )
 }
 
-const Receita = ({ imagem, nome, favorito}) => {
-    const navigation = useNavigation();
+const Receita = () => {
+    const [isLoading, setLoading] = useState(true);
+    const [receitas, setReceitas] = useState([]);
+
+    const getReceitas = async () => {
+        try {
+            const response = await fetch('https://receitas-pweb2.onrender.com/receitas');
+            const json = await response.json();
+            setReceitas(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getReceitas();
+    }, []);
 
     return (
-        <Pressable onPress={() => navigation.navigate('Segunda')}>
-        <View style={{ marginTop: 45, marginLeft: 45, }}>
-
-            <Image style={{ width: 321, height: 165.15, borderTopLeftRadius: 4 , borderTopRightRadius: 4,  }} source={{ uri: imagem }}></Image>
-
-            <View style={{ flexDirection: 'row' }}>
-                <Text style={{
-
-                    fontFamily: 'Roboto_400Regular',
-                    width: 237,
-                    height: 85,
-                    fontSize: 19,
-                    fontWeight: 400,
-                    paddingTop: 10,
-                    paddingLeft: 10,
-                    backgroundColor: '#33241F',
-                    borderBottomLeftRadius: 10,
-                    // borderBottomRightRadius: 10,
-                    color: '#F88B62',
-
-
-                }}>{nome}  </Text>
-
-                <Image style={{ width: 84, height: 85, backgroundColor: '#33241F', borderBottomRightRadius: 10, }} source={{ uri: favorito }}></Image>
-
-            </View>
-
+        <View style={{ flex: 1, padding: 24 }}>
+            {isLoading ? (
+                <ActivityIndicator />
+            ) : (
+                <FlatList
+                    data={receitas}
+                    keyExtractor={({ id }) => id}
+                    renderItem={({ item }) => (
+                        <Text>
+                            {item.imagem}, {item.nomeReceita}
+                        </Text>
+                    )}
+                />
+            )}
         </View>
-        </Pressable>
-    )
-};
-
-const RECEITAS = [
-    { imagem: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/imagem1.png', nome: 'Macarrão á Bolonhesa', favorito: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/heart.png' },
-    // { imagem: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/imagem1.png', nome: 'Macarrão á Bolonhesa', favorito: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/heart.png' },
-    // { imagem: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/imagem1.png', nome: 'Macarrão á Bolonhesa', favorito: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/heart.png' },
-    // { imagem: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/imagem1.png', nome: 'Macarrão á Bolonhesa', favorito: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/heart.png' },
-];
-
-const FlatListBasics = () => {
-    return (
-        <View>
-            <FlatList
-                data={RECEITAS}
-                renderItem={({ item }) =>
-                    <Receita imagem={item.imagem} nome={item.nome} favorito={item.favorito} />
-                }
-            />
-        </View>
-    )
+    );
 };
 
 
@@ -143,7 +127,61 @@ export default Receitas;
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
+// const Receita = ({ imagem, nome, favorito}) => {
+//     const navigation = useNavigation();
 
+//     return (
+//         <Pressable onPress={() => navigation.navigate('Segunda')}>
+//         <View style={{ marginTop: 45, marginLeft: 45, }}>
+
+//             <Image style={{ width: 321, height: 165.15, borderTopLeftRadius: 4 , borderTopRightRadius: 4,  }} source={{ uri: imagem }}></Image>
+
+//             <View style={{ flexDirection: 'row' }}>
+//                 <Text style={{
+
+//                     fontFamily: 'Roboto_400Regular',
+//                     width: 237,
+//                     height: 85,
+//                     fontSize: 19,
+//                     fontWeight: 400,
+//                     paddingTop: 10,
+//                     paddingLeft: 10,
+//                     backgroundColor: '#33241F',
+//                     borderBottomLeftRadius: 10,
+//                     // borderBottomRightRadius: 10,
+//                     color: '#F88B62',
+
+
+//                 }}>{nome}  </Text>
+
+//                 <Image style={{ width: 84, height: 85, backgroundColor: '#33241F', borderBottomRightRadius: 10, }} source={{ uri: favorito }}></Image>
+
+//             </View>
+
+//         </View>
+//         </Pressable>
+//     )
+// };
+
+// const RECEITAS = [
+//     { imagem: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/imagem1.png', nome: 'Macarrão á Bolonhesa', favorito: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/heart.png' },
+//     // { imagem: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/imagem1.png', nome: 'Macarrão á Bolonhesa', favorito: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/heart.png' },
+//     // { imagem: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/imagem1.png', nome: 'Macarrão á Bolonhesa', favorito: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/heart.png' },
+//     // { imagem: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/imagem1.png', nome: 'Macarrão á Bolonhesa', favorito: 'https://raw.githubusercontent.com/AlePereiras/receitasreact/master/img/heart.png' },
+// ];
+
+// const FlatListBasics = () => {
+//     return (
+//         <View>
+//             <FlatList
+//                 data={RECEITAS}
+//                 renderItem={({ item }) =>
+//                     <Receita imagem={item.imagem} nome={item.nome} favorito={item.favorito} />
+//                 }
+//             />
+//         </View>
+//     )
+// };
 
 
 
